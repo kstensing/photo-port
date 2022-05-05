a5a8cd7961c168c403089330e3632acdfc863987
kstensing <stensingk@gmail.com>
2022-05-05 17:18:22 -0500

    added on selected styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
 
@@ -17,6 +19,7 @@ function Nav(props) {
   return (
     <header className="flex-row px-1">
       <h2>
+        
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
@@ -24,27 +27,25 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+          <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick('Contact')}>
-              Contact
-            </span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+          <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name
-                }`}
-              key={category.name}
+          <li
+            className={`mx-1 ${
+            currentCategory.name === category.name && !contactSelected && `navActive`
+            }`}
+            key={category.name}
+          >
+            <span onClick={() => {
+              setCurrentCategory(category);
+              setContactSelected(false);
+            }}
             >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category);
-                }}
-              >
                 {capitalizeFirstLetter(category.name)}
               </span>
             </li>
